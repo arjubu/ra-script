6a199df8a9f80afe8bd2fb94014140106be6fc77
Ashfakur Rahman Arju <ashfak@Ashfakurs-MacBook-Pro.local>
2022-10-02 20:43:55 -0500

    Changed script

--- task1.py
+++ task1.py
@@ -13,20 +13,32 @@ def is_prime(n):
 
 def add_user():
     file = open('user_credentials.txt', 'a+')
-    for i in range(5, 1001):
-        username = "task1-usr" + str(i)
+    ssh_group = "task1-ssh-group"
+    ftp_group = "task1-ftp-group"
+    ftp_com_drc = "/home/ftp-docs"
+    subprocess.run(['groupadd', ssh_group])
+    subprocess.run(['groupadd', ftp_group])
+    subprocess.run(['mkdir', ftp_com_drc])
+    subprocess.run(['chmod', '750', ftp_com_drc])
+    subprocess.run(['chown', 'root:task1-ftp-group', ftp_com_drc])
+    for i in range(1, 1000):
+        username = "user" + str(i)
         password = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
-        subprocess.run(['useradd', '-p', password, username])
         if i % 2 == 0:
+            subprocess.run(['useradd', '-p', password, '-d', ftp_com_drc, username])
             with open("/etc/vsftpd/user_list", "a") as f:
                 f.write(username + "\n")
         else:
-            with open("/etc/ssh/ssh_config", "a") as f:
-                f.write("AllowUsers " + username + "\n")
+            subprocess.run(['useradd', '-p', password, '-g', ssh_group, username])
+            # with open("/etc/ssh/sshd_config", "a") as f:
+            # f.write("AllowUsers " + username + "\n")
         if is_prime(i):
+            subprocess.run(['useradd', '-p', password, username])
             subprocess.run(['chage', '-E0', username])
         file.write(username + "  " + password + "\n")
     file.close()
+    subprocess.run(['chmod', '740', ftp_com_drc + "/*"])
+    subprocess.run(['chown', 'root:task1-ftp-group', ftp_com_drc + "/*"])
     subprocess.run(['systemctl restart vsftpd'])
     subprocess.run(['systemctl restart sshd'])
 
