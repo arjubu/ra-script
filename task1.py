6bb26b34ff99c7a78a96a984cd7b3bf376712109
Ashfakur Rahman Arju <ashfak@Ashfakurs-MacBook-Pro.local>
2022-10-03 19:19:53 -0500

    Changed script

--- task1.py
+++ task1.py
@@ -21,46 +21,49 @@ def add_user():
     # subprocess.run(['mkdir', ftp_com_drc])
     # subprocess.run(['chmod', '750', ftp_com_drc])
     # subprocess.run(['chown', 'root:task1-ftp-group', ftp_com_drc])
-    for i in range(1006, 1009):
+    for i in range(1009, 10012):
         username = "user" + str(i)
         password = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
         adduserstr = 'sudo adduser ' + username + ' --gecos ' + '"' + "First Last,RoomNumber,WorkPhone,HomePhone" + '"' + " --disabled-password "
         print(adduserstr)
-        subprocess.Popen(adduserstr, universal_newlines=True, shell=True,
-                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output, error = subprocess.Popen(adduserstr, universal_newlines=True, shell=True,
+                                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
         userpass = "echo " + '"' + username + ':' + password + '" | sudo chpasswd'
-        print(userpass)
+        print(output)
         subprocess.Popen(userpass, universal_newlines=True, shell=True,
-                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
         if i % 2 == 0:
             adduserftpstr = "sudo adduser " + username + " " + ftp_group
-            print(adduserftpstr)
-            subprocess.Popen(adduserftpstr, universal_newlines=True, shell=True,
-                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+            output, error = subprocess.Popen(adduserftpstr, universal_newlines=True, shell=True,
+                                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
+            print(output)
             # subprocess.run(['sudo adduser', username, ftp_group])
             # subprocess.run(['useradd', '-p', password, '-d', ftp_com_drc, username])
             # with open("/etc/vsftpd/user_list", "a") as f:
             # f.write(username + "\n")
         else:
             addusrsshstr = "sudo adduser " + username + " " + ssh_group
-            print(addusrsshstr)
-            subprocess.Popen(addusrsshstr, universal_newlines=True, shell=True,
-                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+            output, error = subprocess.Popen(addusrsshstr, universal_newlines=True, shell=True,
+                                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
+            print(output)
             # subprocess.run(['useradd', '-p', password, '-g', ssh_group, username])
             # with open("/etc/ssh/sshd_config", "a") as f:
             # f.write("AllowUsers " + username + "\n")
         if is_prime(i):
             # subprocess.run(['useradd', '-p', password, username])
             lockusrstr = "sudo usermod -L " + username
-            subprocess.Popen(lockusrstr, universal_newlines=True, shell=True,
-                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            output, error = subprocess.Popen(lockusrstr, universal_newlines=True, shell=True,
+                                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
+            print(output)
             # subprocess.run(['sudo usermod', '-L', username])
         file.write(username + "  " + password + "\n")
         print("user" + str(i) + " Created............")
     file.close()
     # subprocess.run(['chmod', '740', ftp_com_drc + "/*"])
     # subprocess.run(['chown', 'root:task1-ftp-group', ftp_com_drc + "/*"])
-    #subprocess.run(['systemctl restart vsftpd'])
+    # subprocess.run(['systemctl restart vsftpd'])
     subprocess.run(['systemctl restart ssh'])
 
 
